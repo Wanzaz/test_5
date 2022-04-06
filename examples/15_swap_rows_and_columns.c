a2e8081f4f1ed93249a4788f8891d61e77f5b9bd
Wanzaz <ondra.pazourek@gmail.com>
2022-04-06 17:35:43 +0200

    fixes uncompleted code

--- examples/15_swap_rows_and_columns.c
+++ examples/15_swap_rows_and_columns.c
@@ -3,11 +3,12 @@
 #include <time.h>
 
 /*
-   15. Máme matici typu n × m . Napište funkci, která vezme tuto matici a přehodí v ní první a poslední řádek (sloupec).
-   15. We have a matrix of type n × m. Write a function that takes this matrix and swaps the first and last rows (columns) in it.
- */
+ *    15. Máme matici typu n × m . Napište funkci, která vezme tuto matici a přehodí v ní první a poslední řádek (sloupec).
+ *       15. We have a matrix of type n × m. Write a function that takes this matrix and swaps the first and last rows (columns) in it.
+ *        */
 
-
+void swap_first_column_with_last(int rows, int cols, int mat[rows][cols]);
+void swap_first_row_with_last(int rows, int cols, int mat[rows][cols]);
 void init_random(int rows, int cols, int mat[rows][cols]);
 void list(int rows, int cols, int mat[rows][cols]);
 
@@ -28,8 +29,13 @@ int main()
 
 	printf("---------------------------------------\n");
 
-	fills_array(rows, cols, mat, array);
-	list_array(array, rows*cols);
+	swap_first_row_with_last(rows, cols, mat);
+	list(rows, cols, mat);
+
+	printf("---------------------------------------\n");
+
+	swap_first_column_with_last(rows, cols, mat);
+	list(rows, cols, mat);
 
 	return 0;
 }
@@ -37,15 +43,27 @@ int main()
 
 void swap_first_row_with_last(int rows, int cols, int mat[rows][cols])
 {
-	int i, j;
+	int i, help;
 
-	for(i = 0; i < rows; i++)
+	for(i = 0; i < rows + 1; i++)
 	{
-		for(j = 0; j < cols; j++)
-		{
-
-		}
+		help = mat[0][i];
+		mat[0][i] = mat[rows - 1][i];
+		mat[rows - 1][i] = help;
 	}
+}
+
+void swap_first_column_with_last(int rows, int cols, int mat[rows][cols])
+{
+	int i, j, help;
+
+	for(i = 0; i < rows + 1; i++)
+	{
+		help = mat[i][0];
+		mat[i][0] = mat[i][cols - 1];
+		mat[i][cols - 1] = help;
+	}
+
 }
 
 void init_random(int rows, int cols, int mat[rows][cols])
@@ -69,3 +87,4 @@ void list(int rows, int cols, int mat[rows][cols])
 		printf("\n");
 	}
 }
+
